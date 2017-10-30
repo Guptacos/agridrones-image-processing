a661c9c1c1b4ecf1c837bf3915ce94f029d5929c
Mert Bozfakioglu <mbozfakioglu@Merts-MacBook-Pro.local>
2017-10-30 11:19:26 -0400

    added another test case

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ perspectiveReverse = ['test_images/halfdome-05.png',
     'test_images/halfdome-04.png','test_images/halfdome-03.png',
     'test_images/halfdome-02.png','test_images/halfdome-01.png',
     'test_images/halfdome-00.png']
-affine = ['test_images/t3.jpg','test_images/t4.jpg','test_images/t5.jpg','test_images/t6.jpg']
+affine = ['test_images/t1.jpg','test_images/t2.jpg','test_images/t3.jpg','test_images/t4.jpg','test_images/t5.jpg','test_images/t6.jpg']
 diagonalLeftRight = ['test_images/b0.jpg','test_images/b1.jpg','test_images/b2.jpg','test_images/b3.jpg']
 diagonalRightLeft = ['test_images/b3.jpg','test_images/b2.jpg','test_images/b1.jpg','test_images/b0.jpg']
 
